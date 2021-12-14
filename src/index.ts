058ebaa64f03d6976b0803e2bbb794f6d8abb000
Jesse Jafa <jesse.jafa@gmail.com>
2021-12-14 12:33:33 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import path from "path";
+
+function getFilename(first: string): string {
+  if (isW32()) {
+    return path.win32.basename(first, path.extname(first));
+  }
+
+  return path.basename(first, path.extname(first));
+}
+
+function isW32(): boolean {
+  return process.platform === 'win32';
+}
+export { getFilename }
